842617ff07f3ec11f8fd8583ccc2307f55930c96
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-11-30 22:43:32 +0100

    vue composition api

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('Zelda')">Change name</button>
-    <button @click="changeAge('30')">Change name</button>
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import type Job from "./types/Job";
 
-type Age = number | string;
+//type Age = number | string;
 
 export default defineComponent({
   name: "App",
   components: {},
-  data() {
-    return {
-      name: "Link",
-      age: 25 as Age,
-    };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-    },
-    changeAge(age: Age) {
-      this.age = age;
-    },
+  setup() {
+    //reactive state
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as Age,
+    // });
+    // return { ...toRefs(state) };
+
+    //reference object to HTML object(aka view model)
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
   },
 });
 </script>
