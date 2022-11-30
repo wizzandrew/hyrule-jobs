ca61af028aa1bd2f855bed48eb6f89d2aaeea199
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-11-30 22:15:28 +0100

    ts type inference

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="app">hello, hyrule!</div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">Change name</button>
+    <button @click="changeAge('30')">Change name</button>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+type Age = number | string;
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as Age,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: Age) {
+      this.age = age;
+    },
+  },
+});
+</script>
 
 <style></style>
