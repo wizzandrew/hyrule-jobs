8e975a5b37e2c1fccf856ed87a9b311b9767de0b
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-12-01 21:03:21 +0100

    component functions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <JobList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -8,6 +16,7 @@
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import JobList from "./components/JobList.vue";
 import type Job from "./types/Job";
+import type OrderTerm from "./types/OrderTerm";
 
 //type Age = number | string;
 
@@ -51,9 +60,33 @@ export default defineComponent({
       },
     ]);
 
-    return { jobs };
+    //state for 'order' option of sorting jobs on the list
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
@@ -23,12 +24,17 @@
 import { defineComponent } from "vue";
 import type { PropType } from "vue";
 import type Job from "@/types/Job";
+import type OrderTerm from "@/types/OrderTerm";
 
 export default defineComponent({
   props: {
     jobs: {
       required: true,
       type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
     },
   },
 });
